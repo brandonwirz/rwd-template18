7958da439f0a897fd88beb4643b79ebd9bf13c7e
Brandon Wirz <brandonwirz@Brandons-MacBook-Pro.local>
2018-06-14 02:13:53 -0600

    Updated rwd template with jQuery navigation

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	var n = '#nav'
+	$('#nav-menu').click(function() {
+		if ($(n).hasClass('nav-open')) {
+			$(n).removeClass('nav-open');
+		}
+		else {
+			$(n).addClass('nav-open');
+		}
+	});
+});
